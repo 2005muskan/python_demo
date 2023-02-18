9e2c3d39ca663be73142a78c0138a9934fa11126
2005muskan <muskankushwah131@gmail.com>
2023-02-18 07:32:49 +0530

    simple commit

---
+++ f_string.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter a value1 : "))
+num2 = int(input('Enter a value2 : '))
+
+print("Sum of num1 or num2 : ",num1+num2)
+if num1 == num2:
+    print("are you good")
+
+elif num1 >= num2:
+    name = "muskan"
+    surname = "Kushwah"
+    A = f"i am {name} {surname}"
+
+    print(A)
+elif num1 <= num2:
+    my = 18
+    X = f" My age is {my}"
+    print(X)
+
+else:
+    print("nothing!!!!")
+    
